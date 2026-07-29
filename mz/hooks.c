1b370be09ee8b2a87fcaf8f3fd692b5e3179a8eb
Dax <contact@antoniodavide.dev>
2026-07-29 19:31:36 +0200

    fix(MZ): split decode/emulate hooks

--- mz/hooks.c
+++ mz/hooks.c
@@ -18,7 +18,20 @@ static void _x86_read_com_string(RDContext* ctx, const RDInstruction* instr,
     }
 }
 
-static void _x86_dos_21h(RDContext* ctx, RDInstruction* instr) {
+static void _x86_decode_dos_21h(RDContext* ctx, RDInstruction* instr) {
+    RDRegValue ah;
+    if(!rd_get_regval(ctx, "ah", &ah)) return;
+
+    switch(ah) {
+        case 0x4c: // exit
+            instr->flow = RD_IF_STOP;
+            break;
+
+        default: break;
+    }
+}
+
+static void _x86_emulate_dos_21h(RDContext* ctx, const RDInstruction* instr) {
     RDRegValue ah;
     if(!rd_get_regval(ctx, "ah", &ah)) return;
 
@@ -55,19 +68,15 @@ static void _x86_dos_21h(RDContext* ctx, RDInstruction* instr) {
             break;
         }
 
-        case 0x4c: // exit
-            instr->flow = RD_IF_STOP;
-            break;
-
         default: break;
     }
 }
 
-static void _x86_dos_int_hook(RDContext* ctx, RDInstruction* instr) {
+static void _x86_decode_dos_int_hook(RDContext* ctx, RDInstruction* instr) {
     if(instr->operands[0].kind != RD_OP_CNST) return;
 
     switch(instr->operands[0].cnst) {
-        case 0x21: _x86_dos_21h(ctx, instr); break;
+        case 0x21: _x86_decode_dos_21h(ctx, instr); break;
         case 0x20: // Terminate program
         case 0x27: // Terminate and stay resident
             instr->flow = RD_IF_STOP;
@@ -77,6 +86,17 @@ static void _x86_dos_int_hook(RDContext* ctx, RDInstruction* instr) {
     }
 }
 
+static void _x86_emulate_dos_int_hook(RDContext* ctx,
+                                      const RDInstruction* instr) {
+    if(instr->operands[0].kind != RD_OP_CNST) return;
+
+    switch(instr->operands[0].cnst) {
+        case 0x21: _x86_emulate_dos_21h(ctx, instr); break;
+        default: break;
+    }
+}
+
 void mz_register_dos_hooks(RDContext* ctx) {
-    rd_register_instruction_hook(ctx, "x86.int", _x86_dos_int_hook);
+    rd_register_decode_hook(ctx, "x86.int", _x86_decode_dos_int_hook);
+    rd_register_emulate_hook(ctx, "x86.int", _x86_emulate_dos_int_hook);
 }
